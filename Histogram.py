a46be6b5af234c7f25907dcfcf6c5727075efcc8
myhappydays <seokjun0515@gmail.com>
2024-06-11 16:01:23 +0900

    Data visualization - Histogram

--- Histogram.py
+++ Histogram.py
@@ -1,12 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
+# 데이터 불러오기
 file_path = "E:\\2024 프로그래머스 데이터 분석 데브코스\\데이터 분석\\9주차\\2차프로젝트\\Programmer-Data-Analysis-2nd\\data\\Preprocessed_BankChurners.csv"
 data = pd.read_csv(file_path)
 
+# 결과를 저장할 디렉토리 생성
+output_dir = "E:\\2024 프로그래머스 데이터 분석 데브코스\\데이터 분석\\9주차\\2차프로젝트\\Programmer-Data-Analysis-2nd\\images\\Histogram"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+unique_value_counts = data.nunique()
+
 for column in data.columns:
     plt.figure(figsize=(8, 6))
-    plt.hist(data[column], color='skyblue')
+    if unique_value_counts[column] <= 20:
+        # 유니크한 값의 개수가 20 이하인 경우에는 해당 개수를 히스토그램의 bin 개수로 설정
+        bins = unique_value_counts[column]
+    else:
+        bins = 30  # 유니크한 값의 개수가 20 초과인 경우에는 기본값으로 30개의 bin을 사용
+    plt.hist(data[column], color='skyblue', bins=bins)
     try:
         mean_value = data[column].mean()
         median_value = data[column].median()
@@ -21,4 +35,8 @@ for column in data.columns:
     plt.xlabel(column)
     plt.ylabel('Frequency')
     plt.grid(True)
-    plt.show()
+    output_file_path = os.path.join(output_dir, f'{column}_histogram.png')
+    plt.savefig(output_file_path)
+    plt.close()
+
+print("히스토그램이 성공적으로 저장되었습니다.")
